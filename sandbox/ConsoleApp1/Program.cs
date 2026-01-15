069065a0681c55f16503cf4f875a424247c73e1f
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2026-01-15 17:44:34 +0900

    update: sandbox

--- sandbox/ConsoleApp1/Program.cs
+++ sandbox/ConsoleApp1/Program.cs
@@ -9,26 +9,29 @@ try
 
     using (var writetx = db.BeginWrite())
     {
-        using (var table = writetx.OpenTable<string, Person>("persons"))
-        {
-            table.Insert("alice", new Person("Alice", 18));
-            table.Insert("bob", new Person("Bob", 30));
-            table.Insert("carol", new Person("Carol", 25));
-            table.Insert("dave", new Person("Dave", 40));
-            table.Insert("eve", new Person("Eve", 22));
-        }
+        var table = writetx.OpenTable<string, Person>("persons");
+
+        table.Insert("alice", new Person("Alice", 18));
+        table.Insert("bob", new Person("Bob", 30));
+        table.Insert("carol", new Person("Carol", 25));
+        table.Insert("dave", new Person("Dave", 40));
+        table.Insert("eve", new Person("Eve", 22));
 
         writetx.Commit();
     }
 
     using (var readtx = db.BeginRead())
     {
-        using (var table = readtx.OpenTable<string, Person>("persons"))
+        var table = readtx.OpenTable<string, Person>("persons");
+
+        foreach (var kv in table)
         {
-            foreach (var kv in table.GetRange("alice", "bob"))
-            {
-                Console.WriteLine($"{kv.Key}: {kv.Value}");
-            }
+            Console.WriteLine($"{kv.Key}: {kv.Value}");
+        }
+
+        foreach (var kv in table.GetRange("alice", "carol"))
+        {
+            Console.WriteLine($"{kv.Key}: {kv.Value}");
         }
     }
 }
