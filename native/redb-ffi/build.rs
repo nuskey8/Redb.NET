dc7f949d688f4b9ce18930b25e3559f49e1fde2f
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2026-01-18 13:05:18 +0900

    update: unity package structure

--- native/redb-ffi/build.rs
+++ native/redb-ffi/build.rs
@@ -1,11 +1,17 @@
 fn main() {
-    csbindgen::Builder::new()
+    let builder = csbindgen::Builder::new()
         .input_extern_file("src/lib.rs")
         .csharp_generate_const_filter(|_| true)
         .csharp_dll_name("libredb")
         .csharp_entry_point_prefix("")
         .csharp_namespace("Redb.Interop")
-        .csharp_class_accessibility("public")
+        .csharp_class_accessibility("public");
+
+    builder
         .generate_csharp_file("../../src/Redb.Interop/NativeMethods.g.cs")
+        .unwrap();
+
+    builder
+        .generate_csharp_file("../../src/Redb.Unity/Assets/Redb/Runtime/NativeMethods.g.cs")
         .unwrap();
 }
