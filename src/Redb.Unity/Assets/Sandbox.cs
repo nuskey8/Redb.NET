b4815f174e34484c0edaa682854c9fe78938be14
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2026-01-18 16:37:12 +0900

    feat: add extension packages for Unity

--- src/Redb.Unity/Assets/Sandbox.cs
+++ src/Redb.Unity/Assets/Sandbox.cs
@@ -1,28 +1,41 @@
+using MessagePack;
 using Redb;
+using Redb.MessagePack;
 using UnityEngine;
 
 public class Sandbox : MonoBehaviour
 {
     void Start()
     {
-        using var db = RedbDatabase.Create("test.redb");
+        using var db = RedbDatabase.Create("test.redb")
+            .WithMessagePackSerializer();
 
         using (var tx = db.BeginWrite())
         {
-            using var table = tx.OpenTable<string, string>("my_table");
+            using var table = tx.OpenTable<string, Person>("my_table");
 
-            table.Insert("key1", "value1");
-            table.Insert("key2", "value2");
+            table.Insert("key1", new Person { Name = "Alice", Age = 30 });
+            table.Insert("key2", new Person { Name = "Bob", Age = 25 });
 
             tx.Commit();
         }
 
         using (var tx = db.BeginRead())
         {
-            using var table = tx.OpenTable<string, string>("my_table");
+            using var table = tx.OpenTable<string, Person>("my_table");
 
             Debug.Log($"key1: {table.Get("key1")}");
             Debug.Log($"key2: {table.Get("key2")}");
         }
     }
+}
+
+[MessagePackObject]
+public record Person
+{
+    [Key(0)]
+    public string Name { get; set; }
+
+    [Key(1)]
+    public int Age { get; set; }
 }
